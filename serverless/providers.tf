b77324f6f36cf67bca2620b58c9819c43ccf4d41
ggaefcu <giovanni.g@aefcu.com>
2024-12-01 18:53:19 -0500

    Add files via upload

---
+++ serverless/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.0.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 5.58.0"
+    }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = ">= 0.24.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.6"
+    }
+    archive = {
+      source = "hashicorp/archive"
+    version = ">= 2.2.0" }
+  }
+}
